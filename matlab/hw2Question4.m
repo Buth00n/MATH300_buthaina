0a4e4aa50925a38c2b7258a4fcef82d286cc24df
Buth00n <108756870+Buth00n@users.noreply.github.com>
2022-07-12 11:23:33 -0700

    Update hw2Question4.m

--- matlab/hw2Question4.m
+++ matlab/hw2Question4.m
@@ -1,14 +1,11 @@
 % Question 4:
-a = rand(100,1);
+a = rand(200,1);
 Limit = 20;
-Acu = 0;
+b = 0;
 N = 1;
-while Acu < Limit
-    Acu = Acu + a(N, 1);
+while b < Limit
+    b = b + a(N, 1);
     N = N +1;
 end
 
-ave_count = sum(a)/n;
-disp(ave_count)
-
-
+disp(N - 1);
