2f1eaab75a1ebded396462baa4edeb6ee51434f5
Buth00n <108756870+Buth00n@users.noreply.github.com>
2022-07-12 00:04:50 -0700

    Create hw2question1.m

---
+++ matlab/hw2question1.m
@@ -0,0 +1,29 @@
+%% File name : Math 300 Homework 2
+% Name: Buthaina Al habsi
+% Date : July 5th, 2022.
+
+% Question 1:
+A = [2 -1 4
+     9  6 2 
+     1  3 8];
+B = [1 1 1
+     1 1 1 
+     1 1 1 ];
+x = [3
+     6
+     8];
+y = [5 10 15];
+
+disp(A * B)
+disp(A * x)
+disp(x' * B)
+%disp(B * y) % this one says error "incorrect dimension for matrix multiplication"
+%disp(x * A) % this one says error "incorrect dimension for matrix multiplication"
+disp(x * y)
+disp(y * x)
+%disp(x * y') % this one too!! for incorrect dimension.
+%disp(x. * y) -not sure why this and the next have errors.
+%disp(A. * B.)
+
+
+
